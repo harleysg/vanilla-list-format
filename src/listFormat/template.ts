e2621c0821cda0c7a602e767bc55087f457f24e3
harley santos garzon <harley.santos.garzon@gmail.com>
2023-03-02 01:47:19 -0500

    feat(templates): create template example

---
+++ src/listFormat/template.ts
@@ -0,0 +1,8 @@
+export const tmpl = `<form class="card">
+  <input type="text" name="names"/>
+  <p class="version-es7">
+    Laura, Camila and Alejandra
+  </p>
+  <p class="version-es5"></p>
+  <button disabled>Send</button>
+</form>`
